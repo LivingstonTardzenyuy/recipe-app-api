745a9f7cc329cf42a090b2896dc0b6f81a74bb70
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-10-13 06:30:57 +0100

    added endpoint for recipe add and list

--- app/recipe/api/serializers.py
+++ app/recipe/api/serializers.py
@@ -6,5 +6,13 @@ class RecipeSerializer(serializers.ModelSerializer):
     """ Serializers for Recipe """
     class Meta:
         models = Recipe 
-        fields = "__all__" 
+        fields = ["id", "title", "time_minutes", "price", "link"] 
         read_only_fields = ['id']
+        
+        
+class RecipeDetailsSerializer(RecipeSerializer):
+    """ Serializers for recipe detail view. We are inheriting from RecipeSerializer sine we will use all it's properties """
+    
+    class Meta(RecipeSerializer.Meta):
+        model = Recipe 
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/api/views.py
+++ app/recipe/api/views.py
@@ -1,4 +1,4 @@
-from .serializers import RecipeSerializer
+from .serializers import RecipeSerializer, RecipeDetailsSerializer
 from recipe.models import Recipe
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -6,7 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ View for maanaging recipe APIs """
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailsSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -14,3 +14,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """ Filter the queryset based on the authenticated user """
         return self.queryset.filter(user = self.request.user).order_by('-id')
+    
+    def post(self):
+        """ Overried the creation of a new recipe to belong to the created user """
+        
+    
+    def get_serializer_class(self):
+        """ Return the serializer class for request """
+        if self.action == 'list':
+            return RecipeSerializer
+        return self.serializer_class
+    
+    
+    def perform_create(self, serializer):
+        """ Create a new recipe """
+        user = self.request.user 
+        serializer.save(user=user)
